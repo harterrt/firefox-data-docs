d4ff8275642c80dd91a21b6673bb8b6c9e115aa0
Ryan Harter <harterrt@gmail.com>
2017-05-19 12:05:24 -0400

    Update documentation

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ build_dir='_book'
 # Depends on https://github.com/davisp/ghp-import
 gitbook build . $build_dir && \
 touch $build_dir/.nojekyll && \
-ghp-import -b gh-pages $build_dir && git push origin gh-pages
+ghp-import -b gh-pages $build_dir && \
+git push origin gh-pages
